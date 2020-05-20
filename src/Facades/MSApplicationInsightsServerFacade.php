a88a8db264fbebd609ea03f58e37ab8f9ccb5638
Blerim Cazimi <blerim@burd.dk>
2020-05-20 14:26:16 +0200

    Facade added

---
+++ src/Facades/MSApplicationInsightsServerFacade.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace Sparav\ApplicationInsights\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class MSApplicationInsightsServerFacade extends Facade
+{
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor()
+    {
+        return 'MSApplicationInsightsServer';
+    }
+}
